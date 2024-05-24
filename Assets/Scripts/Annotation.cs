cf4f2ea8457e60a2246ce77e4c4883f35c9a1d01
Marcello Morena <morma016@mymail.unisa.edu.au>
2024-05-24 19:22:30 +0930

    Updated Annotation script. Fixed bug where child objects wouldn't update if new parent colour was set to be same as previously updated colour. Cleaned up duplicate code.

--- Assets/Scripts/Annotation.cs
+++ Assets/Scripts/Annotation.cs
@@ -16,60 +16,45 @@ public class Annotation : MonoBehaviour
         None
     }
 
+    public void setHighlight(Highlight highlight)
+    {
+        this.highlightColour = highlight;
+        if (this.GetComponent<Renderer>() != null)
+        {
+            switch (highlightColour)
+            {
+                case Highlight.Green:
+                    this.GetComponent<Renderer>().material.color = Color.Lerp(originalColour, Color.green, .5f);
+                    break;
+                case Highlight.Yellow:
+                    this.GetComponent<Renderer>().material.color = Color.Lerp(originalColour, Color.yellow, .5f);
+                    break;
+                case Highlight.Red:
+                    this.GetComponent<Renderer>().material.color = Color.Lerp(originalColour, Color.red, .5f);
+                    break;
+                case Highlight.Blue:
+                    this.GetComponent<Renderer>().material.color = Color.Lerp(originalColour, Color.blue, .5f);
+                    break;
+                case Highlight.None:
+                    this.GetComponent<Renderer>().material.color = originalColour;
+                    break;
+            }
+        }
+    }
+
     public void changeHighlightColour(Highlight newHighlightColour)
     {
-        // if input colour is not the same as current colour
-        if (newHighlightColour != highlightColour)
+        highlightColour = newHighlightColour;
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        if(this.GetComponent<Renderer>() != null )
         {
-            highlightColour = newHighlightColour;
-            Renderer[] renderers = GetComponentsInChildren<Renderer>();
-            if(this.GetComponent<Renderer>() != null )
-            {
-                Color oldColour = originalColour;
-                switch (highlightColour)
-                {
-                    case Highlight.Green:
-                        this.GetComponent<Renderer>().material.color = Color.Lerp(oldColour, Color.green, .5f);
-                        break;
-                    case Highlight.Yellow:
-                        this.GetComponent<Renderer>().material.color = Color.Lerp(oldColour, Color.yellow, .5f);
-                        break;
-                    case Highlight.Red:
-                        this.GetComponent<Renderer>().material.color = Color.Lerp(oldColour, Color.red, .5f);
-                        break;
-                    case Highlight.Blue:
-                        this.GetComponent<Renderer>().material.color = Color.Lerp(oldColour, Color.blue, .5f);
-                        break;
-                    case Highlight.None:
-                        this.GetComponent<Renderer>().material.color = oldColour;
-                        break;
-                }
-            }
+            this.setHighlight(newHighlightColour);
+        }
 
-            // loop through
-            foreach (Renderer renderer in renderers)
-            {
-                Color oldColour = renderer.gameObject.GetComponent<Annotation>().originalColour;
-
-                switch (highlightColour)
-                {
-                    case Highlight.Green:
-                        renderer.material.color = Color.Lerp(oldColour, Color.green, .5f);
-                        break;
-                    case Highlight.Yellow:
-                        renderer.material.color = Color.Lerp(oldColour, Color.yellow, .5f);
-                        break;
-                    case Highlight.Red:
-                        renderer.material.color = Color.Lerp(oldColour, Color.red, .5f);
-                        break;
-                    case Highlight.Blue:
-                        renderer.material.color = Color.Lerp(oldColour, Color.blue, .5f);
-                        break;
-                    case Highlight.None:
-                        renderer.material.color = oldColour;
-                        break;
-                }
-            }
+        // loop through
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.gameObject.GetComponent<Annotation>().setHighlight(highlightColour);
         }
     }
 
